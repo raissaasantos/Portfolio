9e4b04bde5cc02933a50264a65786f3d7b8e4661
raissaasantos <raissasantos55@yahoo.com>
2024-06-27 00:16:42 -0300

    Profile: finished

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('cv-button');
+
+button.addEventListener('mouseover', function() {
+    button.style.backgroundColor = '#B3D9D9';
+    button.style.color = '#000000';
+});
+
+button.addEventListener('mouseout', function() {
+    button.style.backgroundColor = '#1E90FF';
+    button.style.color = '#FFFFFF';
+});
