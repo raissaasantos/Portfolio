05b644dbcd955142d12dbc22ed9ceb62a23f40cb
raissaasantos <raissasantos55@yahoo.com>
2024-07-22 22:44:39 -0300

    Progress

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-const button = document.getElementById('cv-button');
+const buttons = document.getElementsByClassName('btn-dark');
+
+// Convertendo a coleção HTMLCollection em um array para iterar sobre cada botão
+Array.from(buttons).forEach(button => {
+    // Adicionando evento 'mouseover' para cada botão
+    button.addEventListener('mouseover', function() {
+        this.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--color-tertiary');
+        this.style.color = getComputedStyle(document.documentElement).getPropertyValue('--color-primary');
+    });
+});
+
+
+/*const button = document.getElementsByClassName('btn-dark');
 
 button.addEventListener('mouseover', function() {
     button.style.backgroundColor = '#B3D9D9';
@@ -8,6 +20,6 @@ button.addEventListener('mouseover', function() {
 button.addEventListener('mouseout', function() {
     button.style.backgroundColor = '#1E90FF';
     button.style.color = '#FFFFFF';
-});
+});*/
 
 
